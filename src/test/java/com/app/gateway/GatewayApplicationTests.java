4b804fc186e149453885d887282519dd30750adf
Moises Molina <w7moises@gmail.com>
2022-09-10 13:17:47 -0500

    url fixed

--- src/test/java/com/app/gateway/GatewayApplicationTests.java
+++ src/test/java/com/app/gateway/GatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.demo.config.server;
+package com.app.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServerApplicationTests {
+class GatewayApplicationTests {
 
     @Test
     void contextLoads() {
